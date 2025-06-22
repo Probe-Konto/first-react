061fafe8cd131fb5bd54dcd10b595c8ab7f0eba3
Probe-Konto <entwickl.konto@gmail.com>
2025-06-22 23:14:23 +0300

    fixing deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/first-react/",
-  plugins: [tailwindcss(), react(), ghPages()],
+  plugins: [tailwindcss(), react()],
 });
